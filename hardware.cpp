63161a6655a86a80fc02e9c755966781e2ac72e3
Dongrang072 <robot4097@gmail.com>
2026-04-29 13:51:50 +0900

    Feat auto ms in config.h

--- hardware.cpp
+++ hardware.cpp
@@ -4,40 +4,38 @@
 static bool currentlyLocked = true;
 static unsigned long unlockTime = 0;
 
-void setupHardware()
-{
+void setupHardware() {
     pinMode(RELAY_PIN, OUTPUT);
-    closeLock(); // 초기 상태는 잠김
+    closeLock();
+    
     pinMode(BUZZER_PIN, OUTPUT);
-    Serial.println("[HW] Relay initialized. Lock is CLOSED.");
+    
+    Serial.println("[HW] Relay + Buzzer initialized. Lock is CLOSED.");
 }
 
-void openLock()
-{
+void openLock() {
     Serial.println(">>> RELAY OFF (전류 차단 → 열림)");
     digitalWrite(RELAY_PIN, UNLOCK_SIGNAL);
     currentlyLocked = false;
     unlockTime = millis();
-
+    
     playUnlockSound();
 }
 
-void closeLock()
-{
+void closeLock() {
     Serial.println(">>> RELAY ON (전류 인가 → 잠김)");
     digitalWrite(RELAY_PIN, LOCK_SIGNAL);
     currentlyLocked = true;
+    
+    playLockSound();
 }
 
-bool isLocked()
-{
+bool isLocked() {
     return currentlyLocked;
 }
 
-void checkAutoLock()
-{
-    if (!currentlyLocked && (millis() - unlockTime >= AUTO_LOCK_MS))
-    {
+void checkAutoLock() {
+    if (!currentlyLocked && (millis() - unlockTime >= AUTO_LOCK_MS)) {
         Serial.println("[AUTO-LOCK] Timeout, locking...");
         closeLock();
     }
